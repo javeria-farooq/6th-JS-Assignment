68b6165e07b532a98513f2b2c662be755bcd2058
javeria-farooq <gettingeducation6@gmail.com>
2024-01-15 20:18:31 +0500

    6th JS Assignment

---
+++ app.js
@@ -0,0 +1,234 @@
+                    // Chapter #1 ALERT
+
+// alert("WELCOME to our website!")
+
+// var password_alert = "<h1> Javascript Alert </h1>" + "<br>" + "Error! Please enter a valid password."
+// document.write(password_alert)
+
+// var password_alert = "JavaScript Alert \n Error! Please enter a valid password."
+// alert(password_alert)
+
+// alert("( Hint: Use line break )")
+
+// alert("Welcome to Js Land... \n Happy Coding!")
+
+
+                //   Chapter #2 VARIIABLES for String
+// 01
+// let username;
+// 02
+// let myName = "Javeria Farooq"
+// 03
+// let message = ' "Hello World" '
+// alert(message)
+// 04
+// let name1 = " Ali Khan"
+// let age = " 18 "
+// let institue = " SMIT"
+// let course = " Web and mobile application development, "
+// alert(name1)
+// alert(age)
+// alert(institute)
+// alert(course)
+// 05
+// let pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(pizza)
+
+// 06 Incomplete
+// let num_child = 3
+// let partner_name = "Farooq and Yasmeen \n Afnan \n Aleeza \n Javeria"
+// let location = "Malir Khokhrapar Karachi"
+// let job = "Booker of Peek Freanz biscuits"
+
+// 08
+// let book = " 'A smarter way to learn JavaScript'"
+// alert("I am trying to learn from the book" + book)
+
+// 09
+// alert("My name is" + name1)
+// alert("I am" + age + "years old")
+// alert("I studied in" + institue)
+// alert("I take" + course + "Module a classes")
+
+// document.write("Yay! I can write HTML content through JavaScript")
+
+
+            //    Chapter #3 Variable for number
+// 01
+// let age = 20
+// alert("I am" + " " + age + " " + " years old")
+
+// 02
+// let visitor_track = 21
+// alert("you have visited this site " + visitor_track + " times")
+
+// 03
+// let birthYear = 2003
+// document.write("My birth year is " + birthYear + "." + "<br>" + "Data type of my declared variable is number")
+
+// 04
+// let visitorName = "Sami Khan "
+// let productTitle = " T-shirts "
+// let quantity = 5
+// document.write(visitorName + "ordered " + quantity + productTitle + "on XYZ clothing store")
+
+        //   chapter #4 variable for legal and illegal
+// 01
+// let _abc;
+// let $abc;
+// let abc;
+
+// 02
+// legal variable:
+// let abc;
+// let xyz2;
+// let de1f;
+// let _jkl;
+// let $pqr;
+// illegal variable
+// let 2ndname;
+// let -sub;
+// let #tag;
+// let .first_var;
+// let @email;
+
+// 04
+// let heading = "<h1> Rules for naming JS variables </h1>"
+// let rule1 = "1) variable names can only contain 'characters', 'numbers', 'alphabets', 'camel case' <br>"
+// let rule2 = "2) Variables must begin with a '$' , '_ ', 'alphabet'.<br>"
+// let rule3 = "3) Variables are case of 'camel'.<br>"
+// let rule4 = "4) Variables should not be JS keywords.<br>"
+// document.write(heading)
+// document.write(rule1)
+// document.write(rule2)
+// document.write(rule3)
+// document.write(rule4)
+
+        //   Chapter #5 Math expressions
+// 01
+// let num1 = 2
+// let num2 = 5
+// let res1 = num1 + num2
+// 02
+// let res2 = num1 - num2
+// let res3 = num1 * num2
+// let res4 = num1 / num2
+// let res5 = num1 % num2 
+// alert("Sum of 2 and 5 is : " + res1)
+// alert("Result of subtracting 5 from 2 is : " + res2)
+// alert("Result of multiplying 2 and 5 is : " + res3)
+// alert("Result of dividing 2 by 5 is : " + res4)
+// alert("Remainder of dividing 2 by 5 is : " + res5)
+
+// 03
+// a)
+// let num;
+// b)
+// document.write("Value after variable declaration is " + num + "<br>")
+// c)
+// num = 3
+// d)
+// document.write("Initial value: " + num + "<br>")
+// e)
+// num = ++num
+// f)
+// document.write("value after increment is: " + num + "<br>")
+// g)
+// num = num + 7
+// h)
+// document.write("value after addition is: " + num + "<br>")
+// i)
+// num = --num
+// j)
+// document.write("value after decrement is : " + num + "<br>")
+// k)
+// num = num % 3
+// l)
+// document.write("The remainder is " + num)
+
+// 04
+// let oneTicket = 600
+// let fiveTickets = 5
+// let res = oneTicket * fiveTickets
+// document.write("Total costs to buy " + fiveTickets + " tickets to a movie is " + res + " PKR.")
+
+// 05
+// let multipler = 4
+// let count1 = 1
+// let count2 = 2
+// let count3 = 3
+// let count4 = 4
+// let count5 = 5
+// let count6 = 6
+// let count7 = 7
+// let count8 = 8
+// let count9 = 9
+// let count10 = 10
+// document.write("Multiplication table of " + multipler + "<br>" + "<br>")
+// document.write(multipler + " x " + count1 + " = " + multipler*count1 + "<br>")
+// document.write(multipler + " x " + count2 + " = " + multipler*count2 + "<br>")
+// document.write(multipler + " x " + count3 + " = " + multipler*count3 + "<br>")
+// document.write(multipler + " x " + count4 + " = " + multipler*count4 + "<br>")
+// document.write(multipler + " x " + count5 + " = " + multipler*count5 + "<br>")
+// document.write(multipler + " x " + count6 + " = " + multipler*count6 + "<br>")
+// document.write(multipler + " x " + count7 + " = " + multipler*count7 + "<br>")
+// document.write(multipler + " x " + count8 + " = " + multipler*count8 + "<br>")
+// document.write(multipler + " x " + count9 + " = " + multipler*count9 + "<br>")
+// document.write(multipler + " x " + count10 + " = " + multipler*count10 + "<br>")
+
+// 07
+// let heading = "<h1> Shopping Cart </h1>"
+// let item1_price = 560
+// let item1_qty = 2
+// let item2_price = 200
+// let item2_qty = 5
+// let shipCharge = 150
+// let totalCost = item1_price * 2 + item2_price * 5 + shipCharge
+
+// document.write(heading + "<br>"+ "<br>")
+// document.write("Price of item 1 is " + item1_price + "<br>")
+// document.write("Quantity of item 1 is " + item1_qty + "<br>")
+// document.write("Price of item 2 is " + item2_price + "<br>")
+// document.write("Quantity of item 2 is " + item2_qty + "<br>" + "<br>")
+// document.write("Shipping Charges " + shipCharge  + "<br>" + "<br>")
+// document.write("Total cost of your order is " + totalCost + "PKR")
+
+// 08
+// let heading = "<h1> Marks sheet </h1>"
+// let totalMark = 1200
+// let obtMark = 900
+// let percent = obtMark / totalMark * 100
+// document.write(heading + "<br>" + "<br>")
+// document.write("Total Marks : " + totalMark + "<br>")
+// document.write("Marks Obtained : " + obtMark + "<br>")
+// document.write("Percentage : " + percent + "%" + "<br>")
+
+// 09
+// let heading = "<h1> Currency in PKR </h1>"
+// let dollar = 10
+// let riyal = 25
+// let dollar_conv = dollar * 104
+// let riyal_conv = riyal * 28
+// let total_pak_currency = dollar_conv + riyal_conv
+// document.write(heading + "<br>" + "<br>")
+// document.write("Total currency in PKR : " + total_pak_currency)
+
+// 10
+// let num = 5
+// a)
+// num = num + 5
+// b)
+// num = num * 10
+// c)
+// num = num / 2
+// document.write(num)
+
+// 11
+// let heading = "<h1> The Age Calculator </h1>"
+// let birthYear = 2003
+// let currentYear = 2023
+// let age = currentYear - birthYear
+// document.write(heading + "<br>" + "<br>")
+// document.write("Current Year : " + currentYear + "<br>")
+// document.write("Birth Year : " + birthYear + "<br>")
+// document.write("I am either " + age + " or 21 years old.")
